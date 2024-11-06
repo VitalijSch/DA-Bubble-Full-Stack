c8bb96a4ddc10506b81bf7a0147deb0f338600b1
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-11-07 00:27:57 +0100

    create auth and login component and creat html and styling for the component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+    { path: 'auth', redirectTo: 'auth/login', pathMatch: 'full' },
+    {
+        path: 'auth',
+        component: AuthComponent,
+        children: [
+            { path: 'login', component: LoginComponent },
+        ]
+    },
+];
